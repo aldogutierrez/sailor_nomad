b5fbd2da757dec78b88f1bfe168bad264a7c8990
Raza Ghulam <ahmad.abdali001@gmail.com>
2020-11-11 20:47:28 -0800

    SignIn added

--- sailor/src/App.js
+++ sailor/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import Nabvar from './component/layout/Navbar'
-import {BrowserRouter, Router, Switch} from 'react-router-dom'; 
+import {BrowserRouter, Route, Switch} from 'react-router-dom'; 
+import SignInPage from './component/auth/SignInPage'; 
+
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="container App">
         <Nabvar />
+        <Switch>
+          <Route path='/signin' component ={SignInPage} />
+        </Switch>
         </div>
       </BrowserRouter>
       
