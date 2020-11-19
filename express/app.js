465b93d9703df41bb3f9a8fe5227251df954f27e
Raza Ghulam <ahmad.abdali001@gmail.com>
2020-11-19 15:21:24 -0800

    express udpated

--- express/app.js
+++ express/app.js
@@ -12,9 +12,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-<<<<<<< HEAD
 
-=======
 app.get('/signup', (req, res) => {
   email = req.query.email  // true
   pass = req.query.password // true
@@ -28,7 +26,6 @@ app.get('/login', (req, res) => {
 
   res.send(db_conn.login(email,pass))
 })
->>>>>>> f825c982dd948e0b434a714c27ef1eebed908ee0
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
