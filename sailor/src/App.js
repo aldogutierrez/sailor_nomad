24ba0c44ed3f1623d133e79c51b03192c97c31b2
Aldo Gutierrez <aldodario1@hotmail.es>
2020-11-19 08:07:58 -0700

    Modified current UI set-up

--- sailor/src/App.js
+++ sailor/src/App.js
@@ -1,22 +1,37 @@
-import React, { Component } from 'react';
-import Nabvar from './component/layout/Navbar'
-import {BrowserRouter, Route, Switch} from 'react-router-dom'; 
-import SignInPage from './component/auth/SignInPage'; 
+// React imports
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="container App">
-        <Nabvar />
-        <Switch>
-          <Route path='/signin' component ={SignInPage} />
-        </Switch>
-        </div>
-      </BrowserRouter>
-      
-    );
-  }
+// File imports
+import Navbar from "./component/layout/Navbar";
+import Home from './component/pages/Home'
+import Services from "./component/pages/Services";
+import SignUp from "./component/pages/SignUp";
+import About from "./component/pages/About";
+import Login from "./component/pages/Login";
+import './App.css'
+
+class App extends Component
+{
+	render()
+	{
+		return (
+			<> 
+				<Router>
+					<div className="container App">
+						<Navbar />
+						<Switch>
+							<Route path='/' exact component={Home}/>
+							<Route path='/services' component={Services}/>
+							<Route path='/sign-up' component={SignUp}/>
+							<Route path='/about' component={About} />
+							<Route path='/login' component={Login} />
+						</Switch>
+					</div>
+				</Router>
+			</>
+		);
+	}
 }
 
 export default App;
--- sailor/src/component/layout/Navbar.js
+++ sailor/src/component/layout/Navbar.js
@@ -1,20 +1,96 @@
-import React from 'react'; 
-import {Link} from 'react-router-dom'; 
-import SignIn from './SignIn'; 
-import SignOut from './SignOut'; 
+// React imports
+import React, {useState, useEffect} from 'react'; 
+import { Link } from 'react-router-dom'; 
 
+// File imports
+import { Button } from './Button';
+import './Navbar.css'
 
+// Full navbar
+function Navbar() {
+	// This is the event for the 'hamburger' menu as the user clicks on it
+	const [click, setClick] = useState(false);
+	const handleClick = () => setClick(!click);
 
- const Navbar  = () => {
-    return (
-      <nav className='nav-wrapper grey darken-3 '>
-        <div className='container'>
-          <Link to='/' className='brand-logo'>Sailor Nomad</Link>
-          <SignIn />
-          <SignOut />
-        </div>
-      </nav>
-    )
+	// Sets the state for the buttons on the pages
+	const [button, setButton] = useState(true);
+
+	// This helps when resizing the window of the app
+	// the buttons and other headers hide and go under the
+	// 'hamburger' menu
+	const showButton = () => {
+		// Checks for the size of the window
+		if (window.innerWidth <= 960) {
+			setButton(false);
+		}
+		else {
+			setButton(true);
+		}
+	};
+
+	// This is used when the application is accessed on mobile
+	// this will help with the buttons staying where they are
+	useEffect(() => {
+		showButton()
+	}, []);
+
+	// Going back to the button event, we resize the window and hide the button
+	window.addEventListener('resize', showButton);
+
+	// 
+	const closeMobileMenu = () => setClick(false);
+	
+	return (
+		<>
+			<nav className='navbar'>
+				<div className='navbar-container'>
+					<Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+						Sailor Nomad <i className="fas fa-passport"/>
+					</Link>
+
+					{/* This is where we have the 'hamburger' menu */}
+					<div className='menu-icon' onClick={handleClick}>
+						<i className={click ? 'fas fa-times' : 'fa fa-bars'} />
+					</div>
+					{/* If on mobile, clicking the menu turns it into an X */}
+					<ul className={click ? 'nav-menu active' : 'nav-menu'}>
+						{/* Home page */}
+						<li className='nav-item'>
+							<Link to='/' className='nav-links' onClick={closeMobileMenu}>
+								Home
+							</Link>
+						</li>
+						{/* Services page */}
+						<li className='nav-item'>
+							<Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+								Services
+							</Link>
+						</li>
+						{/* About page */}
+						<li className='nav-item'>
+							<Link to='/about' className='nav-links' onClick={closeMobileMenu}>
+								About
+							</Link>
+						</li>
+						{/* Login page */}
+						<li className='nav-item'>
+							<Link to='/login' className='nav-links-mobile' onClick={closeMobileMenu}>
+								Log In
+							</Link>
+						</li>
+						{/* Sign Up page button */}
+						<li className='nav-item'>
+							<Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+								Sign Up
+							</Link>
+						</li>
+					</ul>
+					{button && <Button buttonStyle='btn--outline'>Log In</Button>}
+					{button && <Button buttonStyle='btn--outline'>Sign Up</Button>}
+				</div>
+			</nav>
+		</>
+    );
 }
 
-export default Navbar
+export default Navbar;
