10e7d78d92dce4c5b4d5cd5eff9eca4417077700
Raza Ghulam <ahmad.abdali001@gmail.com>
2020-11-19 15:02:47 -0800

    express modified

--- express/app.js
+++ express/app.js
@@ -1,13 +1,20 @@
 
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express'); 
+const app = express(); 
+const port = 3001; 
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false }));
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+
 
