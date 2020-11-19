783a806b3f7c587dbb7ef33b943dbb951259066b
Raza Ghulam <ahmad.abdali001@gmail.com>
2020-11-19 11:27:49 -0800

    login and signup built

--- sailor/src/component/pages/Login.js
+++ sailor/src/component/pages/Login.js
@@ -1,15 +1,56 @@
 // React imports
-import React from 'react'
+import React, { Component } from 'react'
 
-// File imports
-import '../../App.css'
 
-function Login() {
-    return (
-        <h1 className="login">
-            Login
-        </h1>
-    );
+
+class Login extends Component {
+
+    state = {
+        email: '',
+        password: ''
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+    }
+
+
+    render() {
+        return (
+            
+            <div className='container'>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css" />
+                
+                <form className='white' onSubmit={this.handleSubmit} >
+                    <h5 className='grey-text text-darken-3'>Sign In</h5>
+
+                    <div className='input-field'>
+                        <label htmlFor='email'>Email</label>
+                        <input type='email' id='email' onChange={this.handleChange} />
+                    </div>
+
+                    <div className='input=field'>
+                        <label htmlFor='passworld'>Password</label>
+                        <input type='password' id='password' onChange={this.handleChange} />
+                    </div>
+
+                    <div className='input-field'>
+                        <button className='btn teal lighten-1 z-depth-0'>Login
+            </button>
+                    </div>
+
+                </form>
+            </div>
+        )
+    }
 }
 
 export default Login;
+
